dbb2f5d05c809b6ceb14cb914d5cf63ec3b2d67e
Neo Chang <neo725@gmail.com>
2016-09-19 04:47:32 +0800

    add increment build number

---
+++ hooks/before_prepare/010_increment_build_number.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+// Save hook under `project-root/hooks/before_prepare/`
+//
+// Don't forget to install xml2js using npm
+// `$ npm install xml2js`
+
+var fs = require('fs');
+var xml2js = require('xml2js');
+
+// Read config.xml
+fs.readFile('config.xml', 'utf8', function (err, data) {
+    console.log('-----------------------------------------');
+    console.log('Increment config.xml build number');
+    if (err) {
+        return console.log(err);
+    }
+
+    // Get XML
+    var xml = data;
+
+    // Parse XML to JS Obj
+    xml2js.parseString(xml, function (err, result) {
+        if (err) {
+            return console.warn(err);
+        }
+
+        // Get JS Obj
+        var obj = result;
+
+        if (typeof obj.widget.$.version === 'undefined') {
+            obj.widget.$.version = '0.0.1';
+        } else {
+            var currentVersion = obj.widget.$.version;
+            var curVer = String(obj.widget.$.version).split('.');
+            var versionObj = {
+                major: Number(curVer[0]),
+                minor: Number(curVer[1]),
+                build: Number(curVer[2])
+            };
+            console.log('Current Version: ', versionObj);
+            // Increment build numbers
+            versionObj.build += 1;
+            console.log('Target Version: ', versionObj);
+            var targetVersion = versionObj.major + '.' + versionObj.minor + '.' + versionObj.build;
+            obj.widget.$.version = targetVersion;
+            console.log(currentVersion + ' -> ' + targetVersion);
+        }
+
+        // Build XML from JS Obj
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+
+        // Write config.xml
+        fs.writeFile('config.xml', xml, function (err) {
+            if (err) { return console.warn(err); }
+            console.log('config.xml build number successfully incremented');
+        });
+
+    });
+});
+
+fs.readFile('package.json', 'utf8', function (err, data) {
+    console.log('-----------------------------------------');
+    console.log('Increment package.json build number');
+    if (err) {
+        return console.warn(err);
+    }
+
+    // Get JSON
+    var json = JSON.parse(data);
+
+    if (typeof json.version === 'undefined') {
+        json.version = '0.0.1';
+    } else {
+        var currentVersion = json.version;
+        var curVer = String(json.version).split('.');
+        var versionObj = {
+            major: Number(curVer[0]),
+            minor: Number(curVer[1]),
+            build: Number(curVer[2])
+        };
+        console.log('Current Version: ', versionObj);
+        // Increment build numbers
+        versionObj.build += 1;
+        console.log('Target Version: ', versionObj);
+        var targetVersion = versionObj.major + '.' + versionObj.minor + '.' + versionObj.build;
+        json.version = targetVersion;
+        console.log(currentVersion + ' -> ' + targetVersion);
+    }
+
+    // Write package.json
+    fs.writeFile('package.json', JSON.stringify(json, null, 4), function (err) {
+        if (err) { return console.warn(err); }
+        console.log('package.json build number successfully incremented');
+    });
+});
